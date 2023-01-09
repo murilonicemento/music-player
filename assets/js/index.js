ac71529b60c1bcc1c9d54b35077c8538fb5094d0
murilonicemento <murilo_nascimento22@outlook.com>
2023-01-09 12:53:47 -0300

    Inclusion Finished Project

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// MUSIC
+const btnPlay = document.getElementById("btn-play");
+
+btnPlay.addEventListener("click", () => {
+    const music = document.getElementById("music");
+    music.play();
+})
